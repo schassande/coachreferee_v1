8d69e771510cd236b998a31e035f192037ecf192
CHASSANDE BARRIOZ Sebastien <sebastien.chassande@soprasteria.com>
2019-05-08 22:06:25 +0200

    remove old backend

--- backend/src/common/headers.js
+++
@@ -1,6 +0,0 @@
-exports.httpHeaders = {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Headers': 'Content-Type, X-Amz-Date, AuthorizationToken, X-Api-Key, X-Amz-Security-Token, X-Amz-User-Agent, Access-Control-Allow-Origin, *',
-    'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    'Access-Control-Allow-Credentials': false // Required for cookies, authorization headers with HTTPS
-}
