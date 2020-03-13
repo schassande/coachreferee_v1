cb58ebc08889456bc91fee4f9e651045f4fc0e58
CHASSANDE BARRIOZ Sebastien <sebastien.chassande@soprasteria.com>
2020-03-14 10:56:32 +1100

    version

--- mobile/src/environments/environment.prod.ts
+++ mobile/src/environments/environment.prod.ts
@@ -8,5 +8,5 @@ export const environment = {
     storageBucket: 'refcoach-676e3.appspot.com',
     messagingSenderId: '252857255317'
   },
-  version: '2.6.2-2020-03-10T12:59:26.628Z'
+  version: '2.6.3-2020-03-13T23:51:44.103Z'
 };
